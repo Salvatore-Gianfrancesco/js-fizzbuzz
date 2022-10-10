5f463b7d5d6b7f1d3c2d796aee5981ea42064463
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-10-10 14:50:44 +0200

    added container and grid in html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,28 @@
+const rowEl = document.querySelector(".row");
+
+let newEl = "";
+
 for (let i = 1; i <= 100; i++) {
-    if ((i % 3 == 0) && (i % 5 == 0)){
-        console.log("FizzBuzz");
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+        // console.log("FizzBuzz");
+        newEl = "FizzBuzz";
     } else if (i % 3 == 0) {
-        console.log("Fizz");
+        // console.log("Fizz");
+        newEl = "Fizz";
     } else if (i % 5 == 0) {
-        console.log("Buzz");
+        // console.log("Buzz");
+        newEl = "Buzz";
     } else {
-        console.log(i);
+        // console.log(i);
+        newEl = i;
     }
+
+    // console.log(newEl);
+    newEl = `
+        <div class="col-2">
+            <div class="card py-4">${newEl}</div>
+        </div>
+    `;
+    // console.log(newEl);
+    rowEl.innerHTML += newEl;
 }
